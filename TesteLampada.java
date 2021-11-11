9b00873de0e6c3040a0b0618a74d2bd2c0c78a55
elidio123 <elidio.laureano@fatec.sp.gov.br>
2021-11-11 17:55:24 +0000

    Teste lampada

---
+++ TesteLampada.java
@@ -0,0 +1,17 @@
+
+
+public class TesteLampada {
+    
+    public static void main(String[] args) {
+      Lampada l= new Lampada();
+      
+      l. ligar();
+       if(l.isSituacaoLampada()){
+         System.out.println("Lampada ligada");
+       
+       }
+       else{
+           System.out.println("Lampada desliga");
+       }
+    }
+}
